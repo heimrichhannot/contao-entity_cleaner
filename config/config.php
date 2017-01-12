512cf60c5b5a55849f890e7e69cfb5ad97c95c28
Dennis Patzer <d.patzer@heimrich-hannot.de>
2017-01-12 16:13:31 +0100

    fixd runMonthly bug

--- config/config.php
+++ config/config.php
@@ -15,7 +15,6 @@ $GLOBALS['TL_CRON']['minutely']['runMinutelyEntityCleaner']	= array('HeimrichHan
 $GLOBALS['TL_CRON']['hourly']['runHourlyEntityCleaner']		= array('HeimrichHannot\EntityCleaner\EntityCleaner', 'runHourly');
 $GLOBALS['TL_CRON']['daily']['runDailyEntityCleaner']			= array('HeimrichHannot\EntityCleaner\EntityCleaner', 'runDaily');
 $GLOBALS['TL_CRON']['weekly']['runWeeklyEntityCleaner']		= array('HeimrichHannot\EntityCleaner\EntityCleaner', 'runWeekly');
-$GLOBALS['TL_CRON']['monthly']['runMonthlyEntityCleaner']		= array('HeimrichHannot\EntityCleaner\EntityCleaner', 'runMonthly');
 
 /**
  * Models
