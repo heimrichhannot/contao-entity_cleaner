c3dbaa126cdc6f78fc253073a876ab6d7ca3cfde
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2017-07-19 11:41:48 +0200

    1.0.2, check changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+## [1.0.2] - 2017-07-19
+
+### Added
+- PHP 7 compatibility
--- dca/tl_entity_cleaner.php
+++ dca/tl_entity_cleaner.php
@@ -234,7 +234,7 @@ class tl_entity_cleaner extends \Backend
 			foreach ($GLOBALS['TL_DCA']['tl_entity_cleaner']['fields']['published']['save_callback'] as $callback)
 			{
 				$this->import($callback[0]);
-				$blnVisible = $this->$callback[0]->$callback[1]($blnVisible, $this);
+				$blnVisible = $this->{$callback[0]}->{$callback[1]}($blnVisible, $this);
 			}
 		}
 
